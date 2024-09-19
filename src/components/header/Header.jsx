5699d7e7a811e97e65a0464cef04993838c2d01a
JusRecondo <justina.recondo@gmail.com>
2024-09-19 18:23:57 -0300

    Implement fixed top header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
 import NavBar from '../navbar/NavBar';
 import styles from './Header.module.scss';
 
 const Header = () => {
     return (
         <header className={styles.header}>
-            <h1>jus recondo</h1>
+            <Link to="/">
+                <h1>jus recondo</h1>
+            </Link>
             <NavBar />
         </header>
     )
